3b6d5759e83a4d0e070d4e4d05691650b6ff27c5
Mauro <mauro.ciccullo@hotmail.com>
2022-11-14 04:10:15 +0100

    feat: skills animations

--- src/components/Pages/SkillsComponent.vue
+++ src/components/Pages/SkillsComponent.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="skills" class="body-container">
     <div class="body-content skills">
-      <div class="body-content skills wrapper">
-        <transition>
-          <div @click="this.toggleSkills" class="body-content skills wrapper el title">
-            <h1>Front end skills</h1>
+        <div class="body-content skills wrapper">
+           <div @click="this.toggleSkills" class="body-content skills wrapper el title">
+            <h1 :class="(togglerSkills) ? '' : 'bounce2'">Front end skills</h1>
           </div>
-        </transition>
         <transition-group>
           <div v-if="togglerSkills" class="body-content skills wrapper el description">
-            <div v-for="(el) in listLogos" :key="el.name" class="skill">
+            <div v-for="(el) in listLogos" :key="el.name" class="skill skill-enter-to">
               <img  :src="el.url" :alt="'logo'+el.name" class="skill-logo"/>
             </div>
           </div>
@@ -79,19 +77,51 @@ export default {
 </script>
 
 <style scoped>
-  .skill{
+
+  .skill-logo{
     width: 4rem;
   }
-  .skill-logo{
-    width: 100%;
+  .skill{
+    width: 4rem;
   }
   .v-enter-active,
   .v-leave-active {
     transition: opacity 0.5s ease;
   }
 
+  .skill {
+    transition: all 0.5s;
+    width: 4rem;
+  }
+  .skill-enter, .skill-leave-to
+    /* .skill-leave-active for <2.1.8 */ {
+    opacity: 0;
+    transform: scale(0);
+  }
+  .skill-enter-to {
+    opacity: 1;
+    transform: scale(1);
+  }
+
+  .skill-leave-active {
+    /*position: absolute;*/
+  }
+
+  .skill-move {
+    opacity: 1;
+    transition: all 0.5s;
+  }
   .v-enter-from,
   .v-leave-to {
     opacity: 0;
+  }
+
+  .bounce2 {
+    animation: bounce2 2s ease infinite;
+  }
+  @keyframes bounce2 {
+    0%, 20%, 50%, 80%, 100% {transform: translateY(0);}
+    40% {transform: translateY(-30px);}
+    60% {transform: translateY(-15px);}
   }
 </style>
